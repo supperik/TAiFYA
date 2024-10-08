68e46b26637c7da113c86e472c5c41059be1d846
supperik <mr.niki234@mail.ru>
2024-10-09 01:24:35 +0300

    Lab1 написан перевод мура в мили вместе с парсером для автомата мура

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,10 @@
-from convert_machine import read_file_and_fill_lists, convert_mili_machine_to_mura
+from convert_machine import read_file_mili_and_fill_lists, convert_mili_machine_to_mura, read_file_mura_and_fill_lists, \
+    convert_mura_machine_to_mili
 
+input_file_mili = 'input.txt'
+input_file_mura = 'input1.txt'
 
-input_file = 'input.txt'
+states_mili_equal_mura, states_mura, states_mili_has_transitions, states_mura_has_transitions, out_signals = read_file_mili_and_fill_lists(input_file_mili)
+convert_mili_machine_to_mura(states_mili_equal_mura, states_mura, states_mili_has_transitions, states_mura_has_transitions, out_signals)
 
-all_transitions_mili = []
-states_mili = []
-states_mili_equal_mura = {}
-states_mura = []
-states_mura_has_transitions = {}
-states_mili_has_transitions = {}
-
-read_file_mili_and_fill_lists(input_file, all_transitions_mili, states_mili, states_mili_equal_mura, states_mura,
-                         states_mura_has_transitions, states_mili_has_transitions)
-
-convert_mili_machine_to_mura(states_mili_equal_mura, states_mura, states_mili_has_transitions)
+convert_mura_machine_to_mili(read_file_mura_and_fill_lists(input_file_mura))
